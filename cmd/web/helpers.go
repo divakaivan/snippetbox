02d65e7d74988edb5d017e3e2e027ebd3e5dc739
divakaivan <timee98@outlook.com>
2025-04-14 18:49:10 +0900

    write template to buffer to check for errors and then to user

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -22,4 +23,27 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 
 func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
+}
+
+func (app *application) render(w http.ResponseWriter, status int, page string, data *templateData) {
+	ts, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, err)
+		return
+	}
+
+	buf := new(bytes.Buffer)
+
+	// write to the buffer instead of directly to w so that we can detect errors
+	err := ts.ExecuteTemplate(buf, "base", data)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	// if the template is written to the buffer without any errors, its ok
+	w.WriteHeader(status)
+
+	buf.WriteTo(w)
 }
