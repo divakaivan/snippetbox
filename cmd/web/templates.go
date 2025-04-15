459a37c28c151ec1f5183f8fc7bf90be032183a4
divakaivan <timee98@outlook.com>
2025-04-15 13:02:53 +0900

    add a Flash field to the data to be accessed through the templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -15,6 +15,7 @@ type templateData struct {
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 	Form        any
+	Flash       string
 }
 
 // create and registering custom template functions
