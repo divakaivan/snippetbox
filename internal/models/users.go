1c6374c88ec883154373db7167b9c72306b79f8a
divakaivan <timee98@outlook.com>
2025-04-16 19:03:33 +0900

    add getting a user method on User

--- internal/models/users.go
+++ internal/models/users.go
@@ -14,6 +14,7 @@ type UserModelInterface interface {
 	Insert(name, email, password string) error
 	Authenticate(email, password string) (int, error)
 	Exists(id int) (bool, error)
+	Get(id int) (*User, error)
 }
 
 type User struct {
@@ -81,8 +82,24 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 
 func (m *UserModel) Exists(id int) (bool, error) {
 	var exists bool
-	stmt := `select exists(select true from users_test where id = ?)`
+	stmt := `select exists(select true from users where id = ?)`
 	err := m.DB.QueryRow(stmt, id).Scan(&exists)
 
 	return exists, err
+}
+
+func (m *UserModel) Get(id int) (*User, error) {
+	var user User
+
+	stmt := `select id, name, email, created from users where id = ?`
+	err := m.DB.QueryRow(stmt, id).Scan(&user.ID, &user.Name, &user.Email, &user.Created)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return &user, nil
 }
